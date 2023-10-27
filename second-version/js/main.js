0b338c34f31d39fd66524f015d618f5250875760
szymonolkowski <szymon.olkowski.up@gmail.com>
2023-10-27 22:08:15 +0200

    css/js

--- second-version/js/main.js
+++ second-version/js/main.js
@@ -7,11 +7,11 @@ function carousel() {
   let i;
   let x = document.getElementsByClassName("slide");
   for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+    x[i].style.display = "none";
   }
   myIndex++;
-  if (myIndex > x.length) {myIndex = 1}    
-  x[myIndex-1].style.display = "block";  
+  if (myIndex > x.length) {myIndex = 1}  
+    x[myIndex-1].style.display = "block";
   setTimeout(carousel, 2000); // Change image every 2 seconds
 }
 
